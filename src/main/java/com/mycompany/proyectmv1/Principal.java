be99f6313e1c5b6d88265ecd14b8812b802435a9
karyme <wendymtz291999@gmail.com>
2020-06-05 12:56:31 -0400

    update

---
+++ src/main/java/com/mycompany/proyectmv1/Principal.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.proyectmv1;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        
+    }
+    
+}
